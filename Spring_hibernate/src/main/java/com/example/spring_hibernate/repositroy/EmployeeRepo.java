b6787bdd72ae504e75e24809fd800898bfcd3533
Isthifa <87200953+Isthifa@users.noreply.github.com>
2023-05-08 15:16:39 +0530

    Delete Spring_hibernate directory

--- Spring_hibernate/src/main/java/com/example/spring_hibernate/repositroy/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.spring_hibernate.repositroy;
-
-import com.example.spring_hibernate.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-
-
-public interface EmployeeRepo extends JpaRepository<Employee,Long> {
-}
